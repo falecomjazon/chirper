3bdac6f93979f32926b98273c7a2a71d12af8a39
Jazon Coelho <falecomjazon@gmail.com>
2023-08-15 22:09:17 -0300

    despachando um evento

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Events\ChirpCreated;
 
 class Chirp extends Model
 {
@@ -13,6 +14,10 @@ class Chirp extends Model
 
     protected $fillable = [
         'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
